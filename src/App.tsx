00432db2adb74bc039157cecca1cef5e84c24469
Whermerson19 <whermersonc@gmail.com>
2021-04-17 09:06:42 -0300

    create global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
+import GlobalStyle from "./styles/Global";
+
 export default function App() {
   return (
-    <h1>app</h1>
+    <>
+      <h1>app</h1>
+      <GlobalStyle />
+    </>
   )
 }
---
+++ src/styles/Global.ts
@@ -0,0 +1,57 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+
+  :root {
+    --background: #121212;
+    --purple: #bb86fc;
+    --white: #fff;
+    --gray: #202024;
+    --black: #000;
+  }
+
+
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+
+  @media (max-width: 1080px) {
+    html {
+      font-size: 93.75%large;
+    }
+  }
+
+  @media (max-width: 720px) {
+    html {
+      font-size: 87.5%;
+    }
+  }
+
+  body {
+    background: var(--background);
+    color: var(--white);
+  }
+
+  html, body, input, button, textarea {
+    font-family: 'Roboto', sans-serif;
+    font-weight: 400;
+    font-size: 1rem;
+  }
+  
+  h1, h2, h3, h4, h5, h6 {
+    font-weight: bold;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  a {
+    text-decoration: none;
+  }
+`;
+
+export default GlobalStyle;
